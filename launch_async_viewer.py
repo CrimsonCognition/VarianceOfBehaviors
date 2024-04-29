cdcdaacc4fc6ca590f5567d54229ce3dc90f7101
CrimsonCognition <150761021+CrimsonCognition@users.noreply.github.com>
2024-04-28 17:24:07 -0700

    Clean-up ish

--- launch_async_viewer.py
+++
@@ -1,8 +0,0 @@
-import multiprocess
-import explore_Viewer
-
-
-def launch_viewer(q, name="Explore", x=5, y=20, size=21, window_height=300):
-    view = explore_Viewer.exploreViewer(q, name, x, y, size, window_height)
-    view.start()
-
